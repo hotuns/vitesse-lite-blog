c3a7939d3e6cb81ec4592e7e93383f1045050f86
hds <hedongshu@foxmail.com>
2022-04-06 18:29:02 +0800

    添加vite插件

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
+import Inspect from 'vite-plugin-inspect'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Markdown from 'vite-plugin-md'
+import Prism from 'markdown-it-prism'
+import LinkAttributes from 'markdown-it-link-attributes'
+
+const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
 
 export default defineConfig({
   resolve: {
@@ -16,11 +24,14 @@ export default defineConfig({
   },
   plugins: [
     Vue({
+      include: [/\.vue$/, /\.md$/],
       reactivityTransform: true,
     }),
 
     // https://github.com/hannoeru/vite-plugin-pages
-    Pages(),
+    Pages({
+      extensions: ['vue', 'md'],
+    }),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
@@ -29,18 +40,52 @@ export default defineConfig({
         'vue/macros',
         'vue-router',
         '@vueuse/core',
+        '@vueuse/head',
       ],
       dts: true,
     }),
 
     // https://github.com/antfu/vite-plugin-components
     Components({
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: true,
+      resolvers: [
+        IconsResolver(),
+      ],
+    }),
+
+    // https://github.com/antfu/vite-plugin-md
+    // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
+    Markdown({
+      wrapperClasses: markdownWrapperClasses,
+      headEnabled: true,
+      markdownItSetup(md) {
+        // https://prismjs.com/
+        md.use(Prism)
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        })
+      },
+    }),
+
+    // https://icones.netlify.app/
+    Icons({
+      autoInstall: true,
     }),
 
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
     Unocss(),
+
+    // https://github.com/antfu/vite-plugin-inspect
+    // Visit http://localhost:3333/__inspect/ to see the inspector
+    Inspect(),
   ],
 
   // https://github.com/vitest-dev/vitest
